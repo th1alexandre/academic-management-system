f159c556eb8517c16290e2f2a81935c6882e141e
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-12-11 21:21:27 -0400

    export dbml to sql

--- src/database/initdb.sql
+++ src/database/initdb.sql
@@ -1,8 +1,74 @@
 GRANT ALL PRIVILEGES ON DATABASE "flask" TO "postgres";
 
 
-CREATE TABLE table_a (
-    ID uuid PRIMARY KEY,
-    column_a varchar(100) NOT NULL,
-    column_b integer NOT NULL
+CREATE TABLE "Pessoa" (
+  "cpf" char(11) PRIMARY KEY,
+  "nome" varchar(128),
+  "data_de_nascimento" date,
+  "contato" uuid,
+  "endereco" uuid
 );
+
+CREATE TABLE "Contato" (
+  "id" uuid PRIMARY KEY,
+  "telefone_1" varchar(14),
+  "telefone_2" varchar(14),
+  "email" varchar(256)
+);
+
+CREATE TABLE "Endereco" (
+  "id" uuid PRIMARY KEY,
+  "cidade" char(7),
+  "uf" char(2),
+  "cep" char(8),
+  "logradouro" varchar(128),
+  "numero" varchar(16),
+  "bairro" varchar(64),
+  "complemento" varchar(256)
+);
+
+CREATE TABLE "Estudante" (
+  "rga" char(12) PRIMARY KEY,
+  "pessoa" char(11),
+  "curso" int,
+  "unidade" int,
+  "contato" uuid
+);
+
+CREATE TABLE "Professor" (
+  "rga" char(12) PRIMARY KEY,
+  "pessoa" char(11),
+  "unidade" int,
+  "contato" uuid
+);
+
+CREATE TABLE "Unidade" (
+  "id" int PRIMARY KEY,
+  "nome" varchar(32)
+);
+
+CREATE TABLE "Curso" (
+  "id" int PRIMARY KEY,
+  "nome" varchar(32),
+  "unidade" int
+);
+
+ALTER TABLE "Pessoa" ADD FOREIGN KEY ("contato") REFERENCES "Contato" ("id");
+
+ALTER TABLE "Pessoa" ADD FOREIGN KEY ("endereco") REFERENCES "Endereco" ("id");
+
+ALTER TABLE "Estudante" ADD FOREIGN KEY ("pessoa") REFERENCES "Pessoa" ("cpf");
+
+ALTER TABLE "Estudante" ADD FOREIGN KEY ("curso") REFERENCES "Curso" ("id");
+
+ALTER TABLE "Estudante" ADD FOREIGN KEY ("unidade") REFERENCES "Unidade" ("id");
+
+ALTER TABLE "Estudante" ADD FOREIGN KEY ("contato") REFERENCES "Contato" ("id");
+
+ALTER TABLE "Professor" ADD FOREIGN KEY ("pessoa") REFERENCES "Pessoa" ("cpf");
+
+ALTER TABLE "Professor" ADD FOREIGN KEY ("unidade") REFERENCES "Unidade" ("id");
+
+ALTER TABLE "Professor" ADD FOREIGN KEY ("contato") REFERENCES "Contato" ("id");
+
+ALTER TABLE "Curso" ADD FOREIGN KEY ("unidade") REFERENCES "Unidade" ("id");
