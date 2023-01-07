4c4e4733e96725444ba55127f8ecdd8dd4ccca72
Th1Alexandre <thiago.freire@th1alexandre.dev>
2023-01-07 17:40:40 -0400

    add: table Pessoa

--- src/database/models/models.py
+++ src/database/models/models.py
@@ -1,5 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Date, String
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Pessoa(Base):
+    __tablename__ = "Pessoa"
+
+    cpf = mapped_column(String(11), primary_key=True)
+    nome = mapped_column(String(128), nullable=False)
+    data_nascimento = mapped_column(Date, nullable=False)
